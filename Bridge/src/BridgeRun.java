019b49f771cb4a149506abd0a4f1b1ecfb4492f8
flopie2009 <florianpiewak@gmx.de>
2015-01-26 21:04:24 +0100

    fist Bridge Commit

---
+++ Bridge/src/BridgeRun.java
@@ -0,0 +1,19 @@
+
+public class BridgeRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//init Light-Sensor rotation
+		
+		//go up to the ramp
+		
+		//switch the light sensor (all the time)
+		
+		//navigate
+		
+		
+
+	}
+
+}
