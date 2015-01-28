ecbaeb64cb1aa8dea1a40ed8d37924585003f32f
flopie2009 <florianpiewak@gmx.de>
2015-01-28 11:26:59 +0100

    Direction und Sweep

--- Bridge/src/LightSwitcher.java
+++ Bridge/src/LightSwitcher.java
@@ -15,25 +15,89 @@ class LightSwitcher extends Thread{
 	final static int rotationAngle = 200;
 	final static int rotationSpeed = 500;
 	final static int rotationSpeedInit = 100;
-	final static int angleLeft = 0;
-	final static int angleMiddle = 95;
-	final static int angleRigth = 190;
+	final static int angleMiddle = 100;
+	final static int angleLeftFull = 0;
+	final static int angleRigthFull = 200;
+	final static int angleLeftMiddle = angleMiddle - 45;
+	final static int angleLeftSmall = angleMiddle - 20;
+	final static int angleRigthMiddle = angleMiddle + 45;
+	final static int angleRigthSmall = angleMiddle + 20;
 	final static NXTRegulatedMotor motor = Motor.A;
 	
+	public enum RotantionRange{
+		SMALL, MIDDLE, FULL
+	}
 	
+	public enum RotantionDirection{
+		Right, Left
+	}
+	
+	private RotantionRange rotationRange;
+	private RotantionDirection rotationDirection;
+	
+	public LightSwitcher() {
+		rotationRange = RotantionRange.FULL;
+		rotationDirection = RotantionDirection.Left;
+	}
+	
+	public void startSweep(){
+		rotationRange = RotantionRange.MIDDLE;
+		super.start();
+	}
+	
+	public void setDirection(RotantionDirection rotationDirection){
+		this.rotationDirection = rotationDirection;
+	}
 	
 	public void run() {
 		try {
 			motor.setSpeed(rotationSpeed);
 			while(true){
-				motor.rotateTo(angleMiddle, true);
-				Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
-				motor.rotateTo(angleRigth, true);
-				Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
-				motor.rotateTo(angleMiddle, true);
-				Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
-				motor.rotateTo(angleLeft, true);
-				Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
+				int actualAngleLeft;
+				int actualAngleRigth;
+				switch (rotationRange) {
+				case SMALL:
+					actualAngleLeft = angleLeftSmall;
+					actualAngleRigth = angleRigthSmall;
+					rotationRange = RotantionRange.MIDDLE;
+					break;
+				case MIDDLE:
+					actualAngleLeft = angleLeftMiddle;
+					actualAngleRigth = angleRigthMiddle;
+					rotationRange = RotantionRange.FULL;
+					break;
+				default:
+					actualAngleLeft = angleLeftFull;
+					actualAngleRigth = angleRigthFull;
+					break;
+				}
+				if(rotationDirection == RotantionDirection.Left){
+					motor.rotateTo(angleMiddle, true);
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
+	//				Thread.sleep(angleMiddle * 1000 / (rotationSpeed * 2));
+					motor.rotateTo(actualAngleRigth, true);
+	//				Thread.sleep((angleMiddle - actualAngleRigth) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2) + 100);
+					motor.rotateTo(angleMiddle, true);
+	//				Thread.sleep((angleMiddle - actualAngleRigth) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
+					motor.rotateTo(actualAngleLeft, true);
+	//				Thread.sleep((actualAngleLeft - angleMiddle) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2) + 100);
+				} else {
+					motor.rotateTo(angleMiddle, true);
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
+	//				Thread.sleep(angleMiddle * 1000 / (rotationSpeed * 2));
+					motor.rotateTo(actualAngleLeft, true);
+	//				Thread.sleep((angleMiddle - actualAngleRigth) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2) + 100);
+					motor.rotateTo(angleMiddle, true);
+	//				Thread.sleep((angleMiddle - actualAngleRigth) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2));
+					motor.rotateTo(actualAngleRigth, true);
+	//				Thread.sleep((actualAngleLeft - angleMiddle) * 1000 / (rotationSpeed));
+					Thread.sleep(rotationAngle * 1000 / (rotationSpeed * 2) + 100);
+				}
 			}
 		} catch (InterruptedException e) {
 			motor.stop();
@@ -64,19 +128,19 @@ class LightSwitcher extends Thread{
 	
 	public static int getRegulatedCurrentAngle(){
 		int angle = motor.getPosition();
-		if(angle < angleLeft)
-			angle = angleLeft;
-		if (angle > angleRigth)
-			angle = angleRigth;
+		if(angle < angleLeftFull)
+			angle = angleLeftFull;
+		if (angle > angleRigthFull)
+			angle = angleRigthFull;
 		return (angle - angleMiddle) * 90 / angleMiddle;
 	}
 	
 	public static double getRegulatedCurrentAngleDouble(){
 		int angle = motor.getPosition();
-		if(angle < angleLeft)
-			angle = angleLeft;
-		if (angle > angleRigth)
-			angle = angleRigth;
+		if(angle < angleLeftFull)
+			angle = angleLeftFull;
+		if (angle > angleRigthFull)
+			angle = angleRigthFull;
 		return (angle - angleMiddle) * 90.0 / angleMiddle;
 	}
 	
