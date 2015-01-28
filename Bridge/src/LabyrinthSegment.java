a88a9b774349af4f3eacddb4f00d396cf818daa1
JimmyMVP <marin.vlastelicap@gmail.com>
2015-01-28 10:29:23 +0100

    Resoliving merge

--- Bridge/src/LabyrinthSegment.java
+++ Bridge/src/LabyrinthSegment.java
@@ -1,11 +1,18 @@
+import lejos.nxt.SensorPort;
+import lejos.nxt.TouchSensor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.robotics.RegulatedMotor;
+import lejos.robotics.Touch;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.util.PilotProps;
 
 public class LabyrinthSegment extends ParcoursSegment {
 
 	public void run() {
-		
+
 	}
-	
+
 	public void stop() {
-		
+
 	}
 }
