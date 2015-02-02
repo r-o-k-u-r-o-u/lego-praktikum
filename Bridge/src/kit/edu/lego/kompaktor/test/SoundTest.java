70b34bc2a9f0614015d9d7f697db8e715b67457c
r-o-k-u-r-o-u <tmansion@gmail.com>
2015-02-02 14:51:58 +0100

    Changes in Disco

---
+++ Bridge/src/kit/edu/lego/kompaktor/test/SoundTest.java
@@ -0,0 +1,19 @@
+package kit.edu.lego.kompaktor.test;
+
+import java.io.File;
+
+import lejos.nxt.Sound;
+
+public class SoundTest {
+
+	public static void main(String[] args) {
+		
+		Sound.playTone(35000, 10);
+		
+		Sound.playSample(new File("lift_music.wav"), 100);7
+		
+		Sound.
+		while (true);
+	}
+
+}
